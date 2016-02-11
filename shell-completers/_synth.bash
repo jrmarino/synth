b58e4bc38022a6e428033ece63a5ca49a9b40149
John Marino <draco@marino.st>
2016-02-11 18:26:53 +0100

    Improve bash completion
    It's better but not great.  I spent hours on this and I'm tired of
    it now.  I'll let somebody that cares improve it more.

--- shell-completers/_synth.bash
+++ shell-completers/_synth.bash
@@ -1,8 +1,40 @@
 #! /usr/bin/env bash
 
+_catport () {
+    local PD CR
+    PD=$1
+    CR=$2
+    first=$3
+    case "${CR}" in
+      "")
+        COMPREPLY=( $( cd ${PD}; compgen -W \
+        "$(/usr/bin/find * -type d -maxdepth 0 -exec echo {}/ \;)" -- ${CR}))
+        ;;
+      */*)
+        COMPREPLY=( $( cd ${PD}; compgen -W \
+        "$(for d in ${CR}*; do [[ -d "$d" ]] && echo $d; done)" -- ${CR}) )
+        ;;
+      *)
+        COMPREPLY=( $( cd ${PD}; compgen -W \
+        "$(/usr/bin/find ${CR}* -type d -maxdepth 1 -depth 1 \
+          -exec echo {}/ \; 2>/dev/null )" -- ${CR}))
+        ;;
+    esac
+    if [ ${first} -eq 1 ]; then
+       if [ -z "${CR}" ]; then
+          COMPREPLY=( ${COMPREPLY[@]} $( cd "$(pwd)"; compgen -W \
+            "$(find * -type f -depth 0 -maxdepth 0 2>/dev/null)" ))
+       else
+          COMPREPLY=( ${COMPREPLY[@]} $(compgen -f -- ${CR}))
+       fi
+    fi
+    return 0;
+}
+
 _synth () {
 
-    local cur prev opts lopts
+    local cur prev opts lopts portsdir
+    portsdir=${PORTSDIR:-/usr/ports}
     COMPREPLY=()
 
     # get command name
@@ -23,13 +55,18 @@ _synth () {
 	status-everything|everything|purge-distfiles|rebuild-repository)
 	    return 0 ;;
 	status|build|just-build|install|force|test)
-	    COMPREPLY=('     space-delimited list of port origins'
-	               '-or- path to file containing one port origin per line')
-	    return 0 ;;
+	_catport ${portsdir} "${cur}" 1
+	return 0 ;;
     esac
+    if [ -d "${portsdir}/${prev}" ]; then
+	_catport ${portsdir} "${cur}" 0
+	return 0;
+    fi
 
-    # if doesn't exist, return opts
-    COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+    if [ ${COMP_CWORD} -eq 1 ]; then
+       # display commands
+       COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+    fi
 }
 
 complete -F _synth synth
