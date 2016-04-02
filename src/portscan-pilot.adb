2d55d7f66b754a2a59d4d3f588346d86ed2f472f
John Marino <draco@marino.st>
2016-04-02 21:06:22 +0200

    Partial revert of prefetch change
    The queue_is_empty condition happens beyond prefetching, and the new
    check causes a behavior change which is wrong.  Revert the check.  This
    means when a build doesn't build or fetch anything, it will still offer
    to rebuild the repository (behavior this check tried to avoid)

--- src/portscan-pilot.adb
+++ src/portscan-pilot.adb
@@ -275,9 +275,6 @@ package body PortScan.Pilot is
         (repository => JT.USS (PM.configuration.dir_repository),
          dry_run    => dry_run, suppress_remote => block_remote);
       bld_counter := (OPS.queue_length, 0, 0, 0, 0);
-      if PKG.queue_is_empty then
-         return False;
-      end if;
       if dry_run then
          return True;
       end if;
