bc21ed4de06c7d48dcaaab6eb8a6a1bd8a5dae46
jrmarino <draco@marino.st>
2016-08-29 15:00:12 -0500

    Bump version to 1.50
    The watchdog is a signficant feature that merits a tens bump.

--- src/definitions.ads
+++ src/definitions.ads
@@ -6,7 +6,7 @@ package Definitions is
    pragma Pure;
 
    synth_version_major : constant String := "1";
-   synth_version_minor : constant String := "43";
+   synth_version_minor : constant String := "50";
    copyright_years     : constant String := "2015-2016";
    host_localbase      : constant String := "/usr/local";
    host_pkg8           : constant String := host_localbase & "/sbin/pkg";
