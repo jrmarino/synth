fe5288589308d162bb8610da5a54df86375bd802
jrmarino <draco@marino.st>
2016-10-15 14:28:52 -0500

    Maintain 3 lines on log between phases
    This matches the variable dump format earlier in the log

--- src/portscan-buildcycle.adb
+++ src/portscan-buildcycle.adb
@@ -521,13 +521,11 @@ package body PortScan.Buildcycle is
    function log_section (title : String; header : Boolean) return String
    is
       hyphens : constant String := (1 .. 50 => '-');
-      --  dashes  : constant String := (1 .. 50 => '=');
    begin
       if header then
          return LAT.LF & hyphens & LAT.LF & "--  " & title & LAT.LF & hyphens;
       else
          return "";
-         --  return dashes;
       end if;
    end log_section;
 
@@ -537,10 +535,8 @@ package body PortScan.Buildcycle is
    ---------------------
    procedure log_phase_end (id : builders)
    is
-      --  dashes  : constant String := (1 .. 80 => '=');
    begin
-      TIO.Put_Line (trackers (id).log_handle, "");
-      --  TIO.Put_Line (trackers (id).log_handle, dashes & LAT.LF);
+      TIO.Put_Line (trackers (id).log_handle, "" & LAT.LF);
    end log_phase_end;
 
 
