18a1e9a76c3d700ae7f956a9c7bb6ef826b54299
jrmarino <draco@marino.st>
2017-03-26 11:11:44 -0500

    Bump version to 1.69
    Items addressed since release 1.68
    * Extend base multiplier for build_depends phase from 5 to 11 minutes
    This is primarily for textlive for which the extraction time on an
    older machine with a spinning disk was measured at 7 minutes.  On that
    machine the watchdog could kick on ports requiring texlive.
    * Increase internal allocation of 28,000 maximum ports to 32,000.  The
    FreeBSD ports tree went over 28k ports recently.  Apparently nobody
    tried to use Synth to build every port on FreeBSD recently because it
    should have failed.
    * Fix an incorrect command exception handler (noticed during the
    development of ravenadm).
    * Augment package architecture determination to recognize ARM and ARM64
    packages.  Synth builds natively on ARM64.
    * Add FAQ entry explaining how to use Synth to crossbuild armv6 (and
    aarch64 for that matter) packages on FreeBSD/ARM64 (contributed by
    Jonathan Chen)
    * Update copyright years on the ISC license

--- src/definitions.ads
+++ src/definitions.ads
@@ -6,7 +6,7 @@ package Definitions is
    pragma Pure;
 
    synth_version_major : constant String := "1";
-   synth_version_minor : constant String := "68";
+   synth_version_minor : constant String := "69";
    copyright_years     : constant String := "2015-2017";
    host_localbase      : constant String := "/usr/local";
    host_make           : constant String := "/usr/bin/make";
