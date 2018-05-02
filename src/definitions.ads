4c926651f15da7eb44737bdfe757583572805293
jrmarino <draco@marino.st>
2018-05-02 10:27:19 -0500

    Bump version

--- src/definitions.ads
+++ src/definitions.ads
@@ -6,7 +6,7 @@ package Definitions is
    pragma Pure;
 
    synth_version_major : constant String := "2";
-   synth_version_minor : constant String := "02";
+   synth_version_minor : constant String := "03";
    copyright_years     : constant String := "2015-2017";
    host_localbase      : constant String := "/usr/local";
    host_make           : constant String := "/usr/bin/make";
