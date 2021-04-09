b5a848f105702baef4250a9fcfaede50c8fd3d08
jrmarino <draco@marino.st>
2021-04-09 06:25:46 -0500

    Bump version and copyright years

--- src/definitions.ads
+++ src/definitions.ads
@@ -6,8 +6,8 @@ package Definitions is
    pragma Pure;
 
    synth_version_major : constant String := "2";
-   synth_version_minor : constant String := "09";
-   copyright_years     : constant String := "2015-2020";
+   synth_version_minor : constant String := "10";
+   copyright_years     : constant String := "2015-2021";
    host_localbase      : constant String := "/usr/local";
    host_make           : constant String := "/usr/bin/make";
    host_pkg8           : constant String := host_localbase & "/sbin/pkg";
